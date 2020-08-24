a70fcea1ffb9abe6bcff32a27bbe0a696c716143
Ubuntu <ubuntu@ip-10-0-3-180.ca-central-1.compute.internal>
2020-08-24 19:18:14 +0000

    2020.08.24

--- src/test/java/com/example/servingwebcontent/GreetingControllerTests.java
+++ src/test/java/com/example/servingwebcontent/GreetingControllerTests.java
@@ -3,11 +3,20 @@ package com.example.servingwebcontent;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import org.yaml.snakeyaml.error.YAMLException;
 
 public class GreetingControllerTests {
     @Test
     public void testGreeting() {
         GreetingController test = new GreetingController();
         assertEquals("greeting",test.greeting("andrew",null));
+    }
+
+    @Test
+    public void breakableTest(){
+        //if x==y
+        int x=0;
+        int y=1;
+        assertEquals(x, y);
     }
 }
